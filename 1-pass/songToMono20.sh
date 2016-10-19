e46b9f8fa0e6bbe741db688f4d8c0b84f6a63a54
Erick Ochoa <eochoa@ualberta.ca>
2016-10-18 20:19:46 -0600

    simplified...

--- 1-pass/songToMono20.sh
+++ 1-pass/songToMono20.sh
@@ -7,60 +7,7 @@ README
 #important variables
 channels=1
 rate=20000
+input=${1}
+output=${2}
 
-input=${1:-x}
-output=${2:-x}
-test "${input}" != x -a "${output}" != x
-areArgumentsSet=$?
-if [ ! $areArgumentsSet ] ; then
-	echo $usage
-fi
-
-test -f "${output}"
-willOutputBeRewritten=$?
-if [ $willOutputBeRewritten ] ; then
-	read -r -d '' errorMsg <<TOPRINT
-Output "${output}" already exists. Please delete the file to proceed.
-TOPRINT
-	(>&2 echo "${errorMsg}")
-	exit -1
-fi
-
-test -f "${input}"
-doesInputExists=$?
-if [ ! $doesInputExists ] ; then
-	read -r -d '' errorMsg <<TOPRINT
-Input file "${input}" does not exist.
-TOPRINT
-	(>&2 echo "${errorMsg}")
-	exit -2
-fi
-
-outputdir=$(basename "${output}")
-test -f "${input}" -a -d "${outputdir}" -a
-doesOutputDirectoryExist=$?
-if [ ! $doesOutputDirectoryExist ] ; then
-	read -r -d errorMsg <<TOPRINT
-Output directory "${outputdir}" does not exist.
-TOPRINT
-	(>&2 echo "${errorMsg}")
-	exit -3
-fi
-
-ffmpegPath=$(which ffmpeg)
-whichFfmpeg=${ffmpegPath:-x}
-test ${whichFfmpeg} != "x"
-doesFfmpegExists=$?
-if [ ! ${doesFfmpegExist} ] ; then
-	read -r -d errorMsg <<TOPRINT
-Command ffmpeg is not installed. Please install to use this script.
-TOPRINT
-	(>&2 echo "${errorMsg}")
-	exit -4
-fi
-
-ffmpeg \
-	-i ${input} \
-	-channels ${channels} \
-	-rate ${rate} \
-	${output}
+ffmpeg -loglevel error -r ${rate} -ac ${channels} -i ${input} ${output}
